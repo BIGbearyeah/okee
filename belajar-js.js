5d639ea77f410532a9b010567176aacd435d2b1b
BIGbearyeah <rofiif0810@gmail.com>
2023-11-11 09:39:24 +0700

    m

---
+++ belajar-js.js
@@ -0,0 +1,14 @@
+document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });
+  
+
+
+
+const makanan = ['pagi', 'siang', 'malem'] 
+
+makanan.push('ayam')
+
+for (let i=0; i<makanan.length; i++) {
+    console.log(makanan[i])
+  }
